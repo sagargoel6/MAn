cf98063e8ddc4cf0dc4c8f156ae11aab1a6986a0
sagargoel6 <sagargoel12@yahoo.in>
2018-03-22 15:37:25 +0530

    Data Frame intro

--- Data structure 1.r
+++ Data structure 1.r
@@ -6,6 +6,7 @@
 (v3 = c(TRUE, FALSE, TRUE)) #logical vector
 (v4 = 100:200) #sequence from 1-10
 (v5 = seq(1,10,2)) #1-10 with an interval of 2 
+
 v4; v5
 v4[1:5] #print the first 5 values
 v4[c(25,3)] # printing values present at the indice of the vector, here indice 25 doesnt exist
@@ -16,3 +17,17 @@ v1[v1=='A'] #Print value if present
 v1[v1=='L'] #Since, L is not present, hence showing character count = 0
 v1[v1 %in% c('A','C')] #List whether A and C are there or not in V1
 v1[v1 %in% c('A','L')] #List whether A and L are present or not and print only present value.
+v7=c('A','L' )
+v1[v1 %in% v7]
+(v8=1:5)
+(names(v8)=c('sagar','arpit','ankit','yash','keshav'))
+v8[2]
+v8
+str(v8) #tells the structure of that objects
+class(v8)
+str(v1)
+
+#Data Frame----
+df=data.frame(rollno=c(1,2,3), name=c('Rohit', 'Lalit', 'Hitesh'), course=c('MBA', 'BBA', 'MCA'), dept=c('dept1', 'dept2', 'dept3'), marks=floor(runif(3,50,100)))
+df #runif command prints random values (number of values, min value, max value)
+

0571b9909080533849197e5910280a36e6e93ec2
sagargoel6 <sagargoel12@yahoo.in>
2018-03-22 15:00:18 +0530

    Vectors

--- first.r
+++ first.r
@@ -5,6 +5,8 @@ mtcars
 #Data structures in R
 
 Vectors
-v1=c('A', 'B', 'C') #character vector
-v2=c(1,2,3) #numeric vector
-v3=c(True, False, True) #logical vector
+v1 = c('A', 'B', 'C') #character vector
+v2 = c(1,2,3) #numeric vector
+v3 = c(True, False, True) #logical vector
+v4 = 1:10 #sequence from 1-10
+v5 = seq(1,10,2) #1-10 with a gap of 2

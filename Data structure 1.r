6c73967275ea489d2493d777482a7e813951f0d5
sagargoel6 <sagargoel12@yahoo.in>
2018-03-22 15:09:23 +0530

    Vectors

--- Data structure 1.r
+++ Data structure 1.r
@@ -2,7 +2,7 @@
 # 4 hyphens ('-') are given to make it a bookmark. 
 
 (v1 = c('A', 'B', 'C')) #character vector
-v2 = c(1,2,3) #numeric vector
-v3 = c(True, False, True) #logical vector
-v4 = 1:10 #sequence from 1-10
-v5 = seq(1,10,2) #1-10 with an interval of 2
+(v2 = c(1,2,3)) #numeric vector
+(v3 = c(TRUE, FALSE, TRUE)) #logical vector
+(v4 = 1:10) #sequence from 1-10
+(v5 = seq(1,10,2)) #1-10 with an interval of 2 

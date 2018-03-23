10b5e8195cee019e663152efb4f92fb4c626ba24
sagargoel6 <sagargoel12@yahoo.in>
2018-03-23 15:01:15 +0530

    Boxplot method of finding outliers

--- Outliers.r
+++ Outliers.r
@@ -5,3 +5,6 @@ marks2 = c(1:80, 100)
 
 summary(marks)
 summary(marks2)
+
+boxplot(marks2) #Box plot shows 1st quantile, median and 3rd quantile as a box
+boxplot(marks) #the value 1000 disintegrates the box plot very much, hence, an outlier 
